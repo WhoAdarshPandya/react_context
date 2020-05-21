5ae1c56b44f902ff74d279f06ed27f7171d67e25
WhoAdarshPandya <adarshpandya0@gmail.com>
2020-05-21 16:11:02 +0530

    ready-to clone

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import store from './store/store'
 
 export default function App() {
-    console.log(store.getState())
     return (
         <div>
             <p>hello world</p>
